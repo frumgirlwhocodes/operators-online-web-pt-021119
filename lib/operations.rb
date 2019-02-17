b67a3388a1823259694f7bdc786db4137713b7c1
lieby weintraub <liebahinda@gmail.com>
2019-02-17 21:48:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
 
-end
+ if speed > 60 
+end	  true
+
+ else speed < 40  
+
+   true 
+  end
 
 
+ def not_safe?(speed)	def not_safe?(speed)
 
-def not_safe?(speed)
-	
-end
-	
+ 	speed > 60 && speed < 40 ? true : false
+end	end
+
 
 
